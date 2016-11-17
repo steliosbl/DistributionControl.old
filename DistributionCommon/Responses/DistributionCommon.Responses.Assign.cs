8b4f797d3f4bbb594b40ca1e6422b24fe28ea7ba
Sty BL <stysdaman@gmail.com>
2016-11-17 10:15:49 +0200

    Implemented response {Assign}.

---
+++ DistributionCommon/Responses/DistributionCommon.Responses.Assign.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Responses
+{
+    public sealed class Assign : Base
+    {
+        public Assign(bool success) : base()
+        {
+            this.Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}
