0494e1e1896eda96df2b83e89474203cf1389275
Sty BL <stysdaman@gmail.com>
2016-11-17 10:21:44 +0200

    Implemented response {Reset}.

---
+++ DistributionCommon/Responses/DistributionCommon.Responses.Reset.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Responses
+{
+    public sealed class Reset : Base
+    {
+        public Reset(bool success) : base()
+        {
+            this.Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}
