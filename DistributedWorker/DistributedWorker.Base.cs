d816fb50a75c54d2ff65218a495367b6e1dca8b0
Sty BL <stysdaman@gmail.com>
2016-11-17 15:12:17 +0200

    Implemented DistributedWorker class {Base}.

---
+++ DistributedWorker/DistributedWorker.Base.cs
@@ -0,0 +1,36 @@
+﻿namespace DistributedWorker
+{
+    public class Base
+    {
+        public readonly int ID;
+        private DistributionCommon.Job.Base job;
+        private System.Threading.Thread workThread;
+
+        public Base(DistributionCommon.Job.Blueprint blueprint)
+        {
+            this.job = new DistributionCommon.Job.Custom(blueprint);
+        }
+
+        public bool Wake()
+        {
+            if (this.workThread == null)
+            {
+                this.workThread = new System.Threading.Thread(() => this.job.Work());
+                this.workThread.Start();
+            }
+
+            return true;
+        }
+
+        public bool Sleep()
+        {
+            if (this.workThread != null)
+            {
+                this.workThread.Abort();
+                this.workThread = null;
+            }
+
+            return true;
+        }
+    }
+}
