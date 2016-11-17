fab56acdd6052b671706994b153929bbcc02ce2c
Sty BL <stysdaman@gmail.com>
2016-11-17 15:12:06 +0200

    Implemented DistributionCommon.Job class {Custom}.

---
+++ DistributionCommon/Job/DistributionCommon.Job.Custom.cs
@@ -0,0 +1,14 @@
+﻿namespace DistributionCommon.Job
+{
+    public class Custom : Base
+    {
+        public Custom(Blueprint blueprint) : base(blueprint)
+        {
+        }
+
+        public override void Work()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
