97dbc2be89ca7f56e635e7b71f32d32b5e76f635
Sty BL <stysdaman@gmail.com>
2016-11-17 15:37:13 +0200

    Implemented DistributionCommon class {DistributionControlException}.

---
+++ DistributionCommon/DistributionCommon.DistributionControlException.cs
@@ -0,0 +1,23 @@
+﻿namespace DistributionCommon
+{
+    using System;
+
+    public sealed class DistributionControlException : Exception
+    {
+        public DistributionControlException() : base()
+        {
+        }
+
+        public DistributionControlException(string message) : base(message)
+        {
+        }
+
+        public DistributionControlException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public DistributionControlException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
