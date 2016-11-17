43c44f77ce178c8ee470c7f60ffb320abf015ee9
Sty BL <stysdaman@gmail.com>
2016-11-17 10:24:16 +0200

    Implemented response {Status}.

---
+++ DistributionCommon/Responses/DistributionCommon.Responses.Status.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Responses
+{
+    public sealed class Status : Base
+    {
+        public Status(bool constructed) : base()
+        {
+            this.Constructed = constructed;
+        }
+
+        public bool Constructed { get; private set; }
+    }
+}
