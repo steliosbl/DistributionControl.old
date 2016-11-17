c3c0efc34ec1e8ee4c0c17f4d058e71749316178
Sty BL <stysdaman@gmail.com>
2016-11-17 10:05:59 +0200

    Implemented request {Sleep}.

---
+++ DistributionCommon/Requests/DistributionCommon.Requests.Sleep.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Requests
+{
+    public sealed class Sleep : Base
+    {
+        public Sleep(int id) : base()
+        {
+            this.ID = id;
+        }
+
+        public int ID { get; private set; }
+    }
+}
