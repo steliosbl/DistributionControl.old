b19ee178d9a5cc3af91ae0a321ab3f793cdbe6cf
Sty BL <stysdaman@gmail.com>
2016-11-17 10:07:13 +0200

    Implemented request {Wake}.

---
+++ DistributionCommon/Requests/DistributionCommon.Requests.Wake.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Requests
+{
+    public sealed class Wake : Base
+    {
+        public Wake(int id) : base()
+        {
+            this.ID = id;
+        }
+
+        public int ID { get; private set; }
+    }
+}
