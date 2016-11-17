d8f8d808444334db543d92b23cb2fc8188078844
Sty BL <stysdaman@gmail.com>
2016-11-17 10:20:25 +0200

    Implemented response {Remove}.

---
+++ DistributionCommon/Responses/DistributionCommon.Responses.Remove.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Responses
+{
+    public sealed class Remove : Base
+    {
+        public Remove(bool success) : base()
+        {
+            this.Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}
