8c491afcb80d85b0e83167927b11da76ce55a551
Sty BL <stysdaman@gmail.com>
2016-11-17 10:22:32 +0200

    Implemented response {Sleep}.

---
+++ DistributionCommon/Responses/DistributionCommon.Requests.Sleep.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Responses
+{
+    public sealed class Sleep : Base
+    {
+        public Sleep(bool success) : base()
+        {
+            this.Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}
