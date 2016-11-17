696c368483725a94765d4c2f7a2f966394cca2fd
Sty BL <stysdaman@gmail.com>
2016-11-17 15:30:38 +0200

    Implemented DistributionCommon class {DependencyManager}.

---
+++ DistributionCommon/DistributionCommon.DependencyManager.cs
@@ -0,0 +1,30 @@
+﻿namespace DistributionCommon
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class DependencyManager
+    {
+        public DependencyManager(string[] dependencies)
+        {
+            this.Depencencies = dependencies;
+        }
+
+        public string[] Depencencies { get; private set; }
+
+        public List<string> FindMissing()
+        {
+            var result = new List<string>();
+            foreach (string file in this.Depencencies)
+            {
+                if (!File.Exists(file))
+                {
+                    result.Add(file);
+                }
+            }
+
+            return result;
+        }
+    }
+}
