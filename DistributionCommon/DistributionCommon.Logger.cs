3b7fc09312b91d070a4d4731b8e8bc5918089448
Sty BL <stysdaman@gmail.com>
2016-11-17 15:27:41 +0200

    Implemented DistributionCommon class {Logger}.

---
+++ DistributionCommon/DistributionCommon.Logger.cs
@@ -0,0 +1,42 @@
+﻿namespace DistributionCommon
+{
+    using System;
+    using System.IO;
+
+    public class Logger
+    {
+        private readonly string filename;
+        private readonly bool outputToConsole;
+
+        public Logger(string filename, bool outputToConsole)
+        {
+            this.filename = filename;
+            this.outputToConsole = outputToConsole;
+            this.Write('\n' + "----------------------------------" + '\n');
+        }
+
+        public void Log(string msg, int severity = 0)
+        {
+            var tags = new string[] { "[INFO] ", "[WARN] ", "[SEVERE] ", "[CRITICAL] " };
+
+            string message = "[" + DateTime.Now.ToString() + "] " + tags[severity] + " " + msg;
+
+            this.Write(message + '\n');
+            if (this.outputToConsole)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        private void Write(string message)
+        {
+            try
+            {
+                File.AppendAllText(this.filename, message);
+            }
+            catch (IOException)
+            {
+            }
+        }
+    }
+}
