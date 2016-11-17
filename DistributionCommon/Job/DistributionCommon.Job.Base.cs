d60c46aba36abc564a4e7f13c3568125cbed661c
Sty BL <stysdaman@gmail.com>
2016-11-17 15:11:48 +0200

    Implemented DistributionCommon.Job class {Base}.

---
+++ DistributionCommon/Job/DistributionCommon.Job.Base.cs
@@ -0,0 +1,14 @@
+﻿namespace DistributionCommon.Job
+{
+    public abstract class Base
+    {
+        public Base(Blueprint blueprint)
+        {
+            this.Blueprint = blueprint;
+        }
+
+        public abstract void Work();
+
+        public Blueprint Blueprint { get; private set; }
+    }
+}
