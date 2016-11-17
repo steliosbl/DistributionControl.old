5963837eec69ddaa42f25111c46d985f4cf067a1
Sty BL <stysdaman@gmail.com>
2016-11-17 10:10:07 +0200

    Implemented response {Base}.

---
+++ DistributionCommon/Responses/DistributionCommon.Responses.Base.cs
@@ -0,0 +1,20 @@
+﻿namespace DistributionCommon.Responses
+{
+    using System;
+
+    public class Base
+    {
+        public Base()
+        {
+            this.RequestType = this.GetType();
+        }
+
+        [Newtonsoft.Json.JsonConstructor]
+        public Base(Type requestType)
+        {
+            this.RequestType = requestType;
+        }
+
+        public Type RequestType { get; private set; }
+    }
+}
