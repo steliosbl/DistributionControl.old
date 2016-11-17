433355798e953ca89bac8f2f1232821fa069dc83
Sty BL <stysdaman@gmail.com>
2016-11-17 10:25:01 +0200

    Implemented response {Wake}.

---
+++ DistributionCommon/Responses/DistributionCommon.Requests.Wake.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Responses
+{
+    public sealed class Wake : Base
+    {
+        public Wake(bool success) : base()
+        {
+            this.Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}
