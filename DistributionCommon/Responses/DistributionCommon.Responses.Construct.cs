b2b6fe09614e14ab99c2501fe28198da7f6bec50
Sty BL <stysdaman@gmail.com>
2016-11-17 10:18:55 +0200

    Implemented response {Construct}.

---
+++ DistributionCommon/Responses/DistributionCommon.Responses.Construct.cs
@@ -0,0 +1,12 @@
+﻿namespace DistributionCommon.Responses
+{
+    public sealed class Construct : Base
+    {
+        public Construct(bool success) : base()
+        {
+            this.Success = success;
+        }
+
+        public bool Success { get; private set; }
+    }
+}
