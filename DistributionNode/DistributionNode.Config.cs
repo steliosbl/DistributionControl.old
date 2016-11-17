6fd55a096a269a7fb6d5e0f9094aa0339fa3d78b
Sty BL <stysdaman@gmail.com>
2016-11-17 15:15:53 +0200

    Implemented DistributionNode class {Config}.

---
+++ DistributionNode/DistributionNode.Config.cs
@@ -0,0 +1,19 @@
+﻿namespace DistributionNode
+{
+    internal sealed class Config
+    {
+        public readonly int Port;
+        public readonly bool Verbose;
+        public readonly bool LiveErrors;
+        public readonly string LogFilename;
+
+        [Newtonsoft.Json.JsonConstructor]
+        public Config(int port, bool verbose, bool liveErrors, string logFilename)
+        {
+            this.Port = port;
+            this.Verbose = verbose;
+            this.LiveErrors = liveErrors;
+            this.LogFilename = logFilename;
+        }
+    }
+}
