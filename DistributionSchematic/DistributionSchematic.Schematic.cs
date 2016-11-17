aa21b1d6f6d8f51d024951f9eff7238ca5d738e0
Sty BL <stysdaman@gmail.com>
2016-11-17 09:28:05 +0200

    Implemented distribution schematic.

---
+++ DistributionSchematic/DistributionSchematic.Schematic.cs
@@ -0,0 +1,26 @@
+﻿namespace DistributionSchematic
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Schematic
+    {
+        public readonly Dictionary<int, Node> Nodes;
+
+        public Dictionary<int, Node> PrimaryNodes
+        {
+            get
+            {
+                return this.Nodes.Where(node => node.Value.Primary).ToDictionary(dict => dict.Key, dict => dict.Value);
+            }
+        }
+
+        public Dictionary<int, Node> SecondaryNodes
+        {
+            get
+            {
+                return this.Nodes.Where(node => !node.Value.Primary).ToDictionary(dict => dict.Key, dict => dict.Value);
+            }
+        }
+    }
+}
