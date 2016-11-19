633a444eb9e0fa6d254717542d8155523b3c5962
Sty BL <stysdaman@gmail.com>
2016-11-19 23:39:15 +0200

    Added Port property to schematic class {Node}.

--- DistributionCommon/Schematic/DistributionSchematic.Node.cs
+++ DistributionCommon/Schematic/DistributionSchematic.Node.cs
@@ -5,14 +5,16 @@
         public readonly int ID;
         public readonly int Slots;
         public readonly System.Net.IPAddress Address;
+        public readonly int Port;
         public readonly bool Primary;
 
         [Newtonsoft.Json.JsonConstructor]
-        public Node(int id, int slots, System.Net.IPAddress address, bool primary)
+        public Node(int id, int slots, System.Net.IPAddress address, int port, bool primary)
         {
             this.ID = id;
             this.Slots = slots;
             this.Address = address;
+            this.Port = port;
             this.Primary = primary;
         }
     }
